ce29fb5b83efee14569b8ea6761e30f17fe46d9d
Peter Kahoun <peter.kahoun@slevomat.cz>
2011-08-08 03:00:16 +0200

    331 moving images
    Signed-off-by: Peter Kahoun <peter.kahoun@slevomat.cz>

--- adminer/adminer.js
+++
@@ -1,3 +0,0 @@
-document.write(unescape("%3Cscript src='jquery.min.js' type='text/javascript'%3E%3C/script%3E")); // http://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js
-document.write(unescape("%3Cscript src='jquery.cookie.js' type='text/javascript'%3E%3C/script%3E"));
-document.write(unescape("%3Cscript src='my.js' type='text/javascript'%3E%3C/script%3E"));
